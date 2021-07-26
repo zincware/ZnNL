63b49d96b138f052cdbb84f8543b8706a57fe2af
SamTov <tovey.samuel@gmail.com>
2021-07-26 20:00:36 +0200

    update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,6 @@ extensions = [
     "IPython.sphinxext.ipython_console_highlighting",
     "IPython.sphinxext.ipython_directive",
     "sphinx.ext.autosectionlabel",
-    "sphinxcontrib.bibtex",
 ]
 
 # Avoid name clashing
