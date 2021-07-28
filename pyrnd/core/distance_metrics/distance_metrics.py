3e1117aa10d2a70394c6eed6f8ce5f755aa9068c
SamTov <tovey.samuel@gmail.com>
2021-07-28 15:53:12 +0200

    Added some parent class modules.

---
+++ pyrnd/core/distance_metrics/distance_metrics.py
@@ -0,0 +1,25 @@
+"""
+This program and the accompanying materials are made available under the terms
+of the Eclipse Public License v2.0 which accompanies this distribution, and is
+available at https://www.eclipse.org/legal/epl-v20.html
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the Zincware Project.
+
+Description: Module for the distance metrics.
+"""
+import tensorflow as tf
+
+
+def cosine_distance(point_1: tf.Tensor, point_2: tf.Tensor):
+    """
+    Compute the cosine similarity distance metric.
+
+    Parameters
+    ----------
+    point_1 : tf.Tensor
+            First point in the comparison.
+    point_2 : tf.Tensor
+            Second point in the comparison.
+    """
+    pass
