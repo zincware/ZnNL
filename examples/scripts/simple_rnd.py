3fd5392ae867ad26c4394dab3c4c15937e49ef55
Konstantin Nikolaou <87869540+KonstiNik@users.noreply.github.com>
2021-12-01 18:44:05 +0100

    adapt execution script. + third plot of membership index included + marking most similar (closest) points after finishing the distillation.

--- examples/scripts/simple_rnd.py
+++ examples/scripts/simple_rnd.py
@@ -18,9 +18,10 @@ if __name__ == "__main__":
     """
     Main method to run the routine.
     """
-    data_generator = znrnd.data.PointsOnCircle(radius=1.0, noise=1e-3)
-    data_generator.build_pool("uniform", n_points=10, noise=False)
-    # data_generator.build_pool(x_points=10, y_points=10)
+    # data_generator = znrnd.data.PointsOnCircle(radius=1.0, noise=1e-3)
+    # data_generator.build_pool("uniform", n_points=10, noise=False)
+    data_generator = znrnd.data.PointsOnLattice()
+    data_generator.build_pool(x_points=10, y_points=10)
 
     target = znrnd.models.DenseModel(
         units=(4, 4, 4),
@@ -40,29 +41,28 @@ if __name__ == "__main__":
     )
 
     agent = znrnd.RND(
-        point_selector=znrnd.point_selection.GreedySelection(threshold=0.1),
-        distance_metric=znrnd.distance_metrics.MahalanobisDistance(
-            data_generator=data_generator,
-            target_network=target,
-            predictor_network=predictor
-        ),
+        point_selector=znrnd.point_selection.GreedySelection(threshold=1),
+        # distance_metric=znrnd.distance_metrics.MahalanobisDistance(
+        #     data_generator=data_generator,
+        #     target_network=target,
+        #     predictor_network=predictor
+        # ),
+        distance_metric=znrnd.distance_metrics.LPNorm(order=2),
         data_generator=data_generator,
         target_network=target,
         predictor_network=predictor,
         tolerance=5,
-        target_size=10,
+        # target_size=10,
     )
     agent.run_rnd()
     target_set = np.array(agent.target_set)
     print(target_set)
-    print(agent.metric_results_storage)
-    # print(agent.metric_results_storage)
 
-    fig, (plot1, plot2) = plt.subplots(2, figsize=(4, 8))
+    fig, (plot1, plot2, plot3) = plt.subplots(3, figsize=(4, 12))
     results = np.array(agent.metric_results_storage)
     plot1.plot(results[:, 0], label='0')
     plot1.plot(results[:, 1], label='1')
-    plot1.plot(results[:, 2], label='2')
+    plot1.plot(results[:, -1], label='2')
 
     # for i, array in enumerate(agent.metric_results_storage):
     #     plot1.plot(array, label=i)
@@ -71,6 +71,17 @@ if __name__ == "__main__":
     plot2.plot(data_generator.data_pool[:, 0], data_generator.data_pool[:, 1], ".")
     plot2.plot(target_set[:, 0], target_set[:, 1], "x", ms=8, mew=3)
     plot2.set_aspect("equal", adjustable="box")
+
+    ind = np.argpartition(1 / agent.metric_results, -10)[-10:]
+    plot2.plot(data_generator.data_pool[:, 0][ind], data_generator.data_pool[:, 1][ind],
+               "_", ms=8, mew=2)
+
+    plot3.plot(1/agent.metric_results, ".")
+    index = []
+    for element in target_set:
+        index.append(np.where((data_generator.data_pool == element).all(axis=1))[0][0])
+    plot3.plot(index, 1/agent.metric_results.numpy()[index], "x", ms=8, mew=3)
+    plot3.plot(ind, 1 / agent.metric_results.numpy()[ind], "_", ms=8, mew=2)
 
     plt.show()
 
