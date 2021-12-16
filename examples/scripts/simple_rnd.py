03fd6ec98b81806e394b2a5f22f62af68afe6937
knikolaou <>
2021-12-16 15:50:26 +0100

    optimizing simple_rnd execution script

--- examples/scripts/simple_rnd.py
+++ examples/scripts/simple_rnd.py
@@ -23,40 +23,46 @@ if __name__ == "__main__":
     data_generator = znrnd.data.PointsOnLattice()
     data_generator.build_pool(x_points=10, y_points=10)
 
+    networks = (2, 24, 24, 24)
+    tolerance = 1e-4
+    loss = znrnd.loss_functions.MeanPowerLoss(order=2)
+
     target = znrnd.models.DenseModel(
-        units=(24, 24, 24),
-        in_d=2,
+        units=networks[1:-1],
+        in_d=networks[0],
         activation='tanh',
-        out_d=24,
-        tolerance=1e-4,
-        loss=znrnd.loss_functions.MeanPowerLoss(order=2)
+        out_d=networks[-1],
+        tolerance=tolerance,
+        loss=loss
     )
     predictor = znrnd.models.DenseModel(
-        units=(24, 24, 24),
-        in_d=2,
+        units=networks[1:-1],
+        in_d=networks[0],
         activation='tanh',
-        out_d=24,
-        tolerance=1e-4,
-        loss=znrnd.loss_functions.MeanPowerLoss(order=2)
+        out_d=networks[-1],
+        tolerance=tolerance,
+        loss=loss
     )
 
     agent = znrnd.RND(
-        point_selector=znrnd.point_selection.GreedySelection(threshold=100),
+        point_selector=znrnd.point_selection.GreedySelection(threshold=1e3),
         # distance_metric=znrnd.distance_metrics.MahalanobisDistance(),
         distance_metric=znrnd.distance_metrics.LPNorm(order=2),
         # distance_metric=znrnd.distance_metrics.HyperSphere(order=2),
+        # distance_metric=znrnd.distance_metrics.CosineDistance(),
         data_generator=data_generator,
         target_network=target,
         predictor_network=predictor,
-        seed_point=[[-5., -5.], [5., 5.], [-5., 5.]],
+        seed_point=[[-5., -5.], [-5., 5.]],
         tolerance=5,
         # target_size=10,
     )
     agent.run_rnd()
     target_set = np.array(agent.target_set)
     print(target_set)
+    print(data_generator.data_pool)
 
-    fig, (plot1, plot2, plot3) = plt.subplots(3, figsize=(4, 12))
+    fig, ax = plt.subplots(3, figsize=(4, 12))
     results = np.array(agent.metric_results_storage)
 
     # Show how biggest loss decreases
@@ -65,26 +71,27 @@ if __name__ == "__main__":
     # plot1.plot(results[:, -1], label='2')
     # plot1.legend()
 
-    plot2.plot(data_generator.data_pool[:, 0], data_generator.data_pool[:, 1], ".")
-    plot2.plot(target_set[:, 0], target_set[:, 1], "x", ms=8, mew=3)
-    plot2.set_aspect("equal", adjustable="box")
+    inv_distance = 1 / agent.metric_results.numpy()
+    map_plot = np.log10(np.flip(inv_distance.reshape(11, 11), axis=0))
+    ax[0].imshow(map_plot)
+
+    ax[1].plot(data_generator.data_pool[:, 0], data_generator.data_pool[:, 1], ".")
+    ax[1].plot(target_set[:, 0], target_set[:, 1], "x", ms=8, mew=3)
+    ax[1].set_aspect("equal", adjustable="box")
 
     add_points = len(target_set) + 30
     ind = np.argpartition(1 / agent.metric_results, -add_points)[-add_points:]
-    plot2.plot(data_generator.data_pool[:, 0][ind], data_generator.data_pool[:, 1][ind],
+    ax[1].plot(data_generator.data_pool[:, 0][ind], data_generator.data_pool[:, 1][ind],
                "_", ms=8, mew=2)
 
-    plot3.plot(1/agent.metric_results, ".")
+    ax[2].plot(1/agent.metric_results, ".")
     index = []
+    # Detect maximum
     for element in target_set:
         index.append(np.where((data_generator.data_pool == element).all(axis=1))[0][0])
-    plot3.plot(index, 1/agent.metric_results.numpy()[index], "x", ms=8, mew=3)
-    plot3.set_yscale("log")
-    plot3.plot(ind, 1 / agent.metric_results.numpy()[ind], "_", ms=8, mew=2)
-
-    data = 1 / agent.metric_results.numpy()
-    data = np.log(np.flip(data.reshape(11, 11), axis=0))
-    plot1.imshow(data)
+    ax[2].plot(index, inv_distance[index], "x", ms=8, mew=3)
+    ax[2].plot(ind, inv_distance[ind], "_", ms=8, mew=2)
+    ax[2].set_yscale("log")
     plt.show()
 
 
