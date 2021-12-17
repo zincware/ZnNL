b0c85776c91d3ceae4afeb80dcabe6265ec52c9a
knikolaou <>
2021-12-17 18:56:13 +0100

    execution script circle_rnd rewritten to calculate mean of many simulations

--- examples/scripts/circle_rnd.py
+++ examples/scripts/circle_rnd.py
@@ -14,24 +14,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-if __name__ == "__main__":
+def main():
     """
     Main method to run the routine.
     """
-    data_generator = znrnd.data.PointsOnCircle(
-        radius=np.arange(2.0, 0.0, -0.2), noise=1e-3
-    )
-    data_generator.build_pool("uniform", n_points=72, noise=False)
 
     # Define both networks
-    networks = (2, 24, 24, 24)
+    networks = (2, 4, 4, 4, 4)
     tolerance = 1e-4
     loss = znrnd.loss_functions.MeanPowerLoss(order=2)
 
     target = znrnd.models.DenseModel(
         units=networks[1:-1],
         in_d=networks[0],
-        activation="tanh",
+        activation="softsign",
         out_d=networks[-1],
         tolerance=tolerance,
         loss=loss,
@@ -39,7 +35,7 @@ if __name__ == "__main__":
     predictor = znrnd.models.DenseModel(
         units=networks[1:-1],
         in_d=networks[0],
-        activation="tanh",
+        activation="softsign",
         out_d=networks[-1],
         tolerance=tolerance,
         loss=loss,
@@ -47,52 +43,90 @@ if __name__ == "__main__":
 
     agent = znrnd.RND(
         point_selector=znrnd.point_selection.GreedySelection(threshold=1e4),
-        distance_metric=znrnd.distance_metrics.LPNorm(order=2),
+        distance_metric=znrnd.distance_metrics.LPNorm(order=1),
         data_generator=data_generator,
         target_network=target,
         predictor_network=predictor,
         seed_point=[
-            data_generator.data_pool[72 * 0 + 0],
-            data_generator.data_pool[72 * 0 + 18],
-            data_generator.data_pool[72 * 0 + 36],
+            # data_generator.data_pool[72 * 0 + 0],
+            # data_generator.data_pool[72 * 3 + 0],
+            # data_generator.data_pool[72 * 7 + 0],
+            # data_generator.data_pool[72 * 0 + 9],
+            # data_generator.data_pool[72 * 3 + 9],
+            # data_generator.data_pool[72 * 7 + 9],
+            # data_generator.data_pool[72 * 0 + 27],
+            # data_generator.data_pool[72 * 3 + 27],
+            # data_generator.data_pool[72 * 7 + 27],
+            data_generator.data_pool[0],
+            data_generator.data_pool[100],
         ],
         tolerance=5,
         # target_size=10,
     )
     agent.run_rnd()
-    target_set = np.array(agent.target_set)
-    print(target_set)
 
-    fig, ax = plt.subplots(2, figsize=(4, 8))
-    results = np.array(agent.metric_results_storage)
+    return agent.target_set, preprocess_data(agent.metric_results.numpy())
 
-    inv_distance = 1 / agent.metric_results.numpy()
-    log_inv_dist = np.log(inv_distance)
 
+def preprocess_data(data):
+    distances = np.array(data)
+    normed_distances = distances / np.amax(distances)
+    membership = 1 - normed_distances
+    exp_membership = np.power(membership, 2)
+    return exp_membership / np.amax(exp_membership)
+
+
+if __name__ == "__main__":
+
+    target_set = []
+    norm_scaled_membership = []
+
+    # data_generator = znrnd.data.PointsOnCircle(
+    #     radius=np.arange(2.0, 0.0, -0.2), noise=1e-3
+    # )
+    # data_generator.build_pool("uniform", n_points=72, noise=False)
+    data_generator = znrnd.data.PointsOnLattice()
+    data_generator.build_pool(x_points=100, y_points=100)
+
+    runs = 0
+    for runs in range(5):
+        results = main()
+        target_set.append(results[0])
+        norm_scaled_membership.append(results[1])
+
+    target_set = np.array(target_set)
+    mean_target_set = target_set.mean(axis=0)
+    norm_scaled_membership = np.array(norm_scaled_membership)
+    mean_norm_scaled_membership = norm_scaled_membership.mean(axis=0)
+    print(mean_target_set)
+
+    fig, ax = plt.subplots(2, figsize=(4, 8), constrained_layout=True)
+
+    ax[0].set_aspect("equal", adjustable="box")
     ax[0].scatter(
         data_generator.data_pool[:, 0],
         data_generator.data_pool[:, 1],
         s=80,
-        c=log_inv_dist,
-        cmap="inferno",
+        c=mean_norm_scaled_membership,
+        cmap="gnuplot2",
     )
-    ax[0].set_aspect("equal", adjustable="box")
+    ax[0].plot(mean_target_set[:, 0], mean_target_set[:, 1], "o", ms=8, mew=1,
+               fillstyle="none")
 
     # ax[1].plot(data_generator.data_pool[:, 0], data_generator.data_pool[:, 1], ".")
     # ax[1].plot(target_set[:, 0], target_set[:, 1], "x", ms=8, mew=3)
     # ax[1].set_aspect("equal", adjustable="box")
 
-    add_points = len(target_set) + 10
-    ind = np.argpartition(inv_distance, -add_points)[-add_points:]
-    # ax[1].plot(data_generator.data_pool[:, 0][ind], data_generator.data_pool[:, 1][ind],
-    #            "_", ms=8, mew=2)
-
-    ax[1].plot(inv_distance, ".")
+    ax[1].set_box_aspect(1 / 1)
+    add_points = len(mean_target_set) + 10
+    ind = np.argpartition(mean_norm_scaled_membership, -add_points)[-add_points:]
     index = []
     # Detect maximum
-    for element in target_set:
+    for element in target_set[0]:
         index.append(np.where((data_generator.data_pool == element).all(axis=1))[0][0])
-    ax[1].plot(index, inv_distance[index], "x", ms=8, mew=3)
-    ax[1].plot(ind, inv_distance[ind], "_", ms=8, mew=2)
-    ax[1].set_yscale("log")
+    ax[1].plot(mean_norm_scaled_membership, ".")
+    ax[1].plot(index, mean_norm_scaled_membership[index], "x", ms=8, mew=3)
+    ax[1].plot(ind, mean_norm_scaled_membership[ind], "_", ms=8, mew=2)
+    # ax[1].set_yscale("log")
+
     plt.show()
