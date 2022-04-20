2d2ef143024c3c5dea6b4597e434c9cdba52985f
SamTov <tovey.samuel@gmail.com>
2022-04-20 08:49:39 +0200

    run black and update requirements.txt

--- CI/unit_tests/core/loss_functions/test_loss_functions.py
+++ CI/unit_tests/core/loss_functions/test_loss_functions.py
@@ -36,8 +36,12 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import jax.numpy as np
 import pytest
 
-from znrnd.core.loss_functions import (AngleDistanceLoss, CosineDistanceLoss,
-                                       LPNormLoss, MeanPowerLoss)
+from znrnd.core.loss_functions import (
+    AngleDistanceLoss,
+    CosineDistanceLoss,
+    LPNormLoss,
+    MeanPowerLoss,
+)
 
 
 class TestLossFunctions:
