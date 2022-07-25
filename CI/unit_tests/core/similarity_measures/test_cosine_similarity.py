a08ce3c171e0c299788d08206588b052e6b0a6f6
knikolaou <>
2022-07-25 16:53:15 +0200

    apply isort on test_cosine_similarity.py

--- CI/unit_tests/core/similarity_measures/test_cosine_similarity.py
+++ CI/unit_tests/core/similarity_measures/test_cosine_similarity.py
@@ -13,7 +13,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import jax.numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 from znrnd.core.similarity_measures.cosine_similarity import CosineSim
 
