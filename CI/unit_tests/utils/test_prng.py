38e0d82d26a8ef0ae4ee3a280ec15c938432cca8
Julian Hossbach <julian.hossbach@gmx.de>
2022-09-22 12:39:49 +0200

    fix CI

--- CI/unit_tests/utils/test_prng.py
+++ CI/unit_tests/utils/test_prng.py
@@ -39,10 +39,10 @@ class TestPRNG:
     def test_deterministic_sequence(self):
         """Test that a given seed produces the same sequence."""
         rng_1 = PRNGKey(42)
-        array_1 = jax.random.uniform(key=rng_1.key, shape=(100,))
+        array_1 = jax.random.uniform(key=rng_1(), shape=(100,))
 
         rng_2 = PRNGKey(42)
-        array_2 = jax.random.uniform(key=rng_2.key, shape=(100,))
+        array_2 = jax.random.uniform(key=rng_2(), shape=(100,))
 
         assert_array_equal(array_1, array_2)
 
@@ -61,11 +61,11 @@ class TestPRNG:
         rng_1 = PRNGKey(42)
         random_list_1 = []
         for i in range(10):
-            random_list_1.append(jax.random.uniform(key=rng_1.key, shape=(10,)))
+            random_list_1.append(jax.random.uniform(key=rng_1(), shape=(10,)))
 
         rng_2 = PRNGKey(42)
         random_list_2 = []
         for i in range(10):
-            random_list_2.append(jax.random.uniform(key=rng_2.key, shape=(10,)))
+            random_list_2.append(jax.random.uniform(key=rng_2(), shape=(10,)))
 
         assert_array_equal(random_list_1, random_list_2)
