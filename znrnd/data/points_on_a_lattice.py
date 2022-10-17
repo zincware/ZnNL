711db7a3b6ec3fdbebb4879afe503741808ca46a
knikolaou <>
2022-10-17 10:00:43 +0200

    Update points_on_a_lattice.py by making it leaner and more flexible.

--- znrnd/data/points_on_a_lattice.py
+++ znrnd/data/points_on_a_lattice.py
@@ -43,7 +43,13 @@ class PointsOnLattice(DataGenerator, ABC):
         """
         self.data_pool = None
 
-    def build_pool(self, x_points: int = 10, y_points: int = 10, boundary: float = -1):
+    def build_pool(
+        self,
+        x_points: int = 11,
+        y_points: int = 11,
+        x_boundary: float = -1,
+        y_boundary: float = -1,
+    ):
         """
         Build the data pool for points on a square lattice with spacing 1.
 
@@ -53,8 +59,12 @@ class PointsOnLattice(DataGenerator, ABC):
                 Number of points in the x direction.
         y_points : int
                 Number of points in the y direction.
-        boundary : float (default = -1)
-                Symmetric boundary of the lattice.
+        x_boundary : float (default = -1)
+                Symmetric boundary of the lattice in x-direction.
+                Defines the min and max value of the lattice.
+                If -1, boundary is scaled by the number of points.
+        y_boundary : float (default = -1)
+                Symmetric boundary of the lattice in y-direction.
                 Defines the min and max value of the lattice.
                 If -1, boundary is scaled by the number of points.
 
@@ -62,16 +72,13 @@ class PointsOnLattice(DataGenerator, ABC):
         -------
         Will call a method which updates the class state.
         """
-        if boundary == -1:
-            x = onp.linspace(
-                -x_points / 2, x_points / 2, x_points + 1, dtype=int
-            ).astype(float)
-            y = onp.linspace(
-                -y_points / 2, y_points / 2, y_points + 1, dtype=int
-            ).astype(float)
-        else:
-            x = onp.linspace(-boundary, boundary, x_points + 1, dtype=float)
-            y = onp.linspace(-boundary, boundary, y_points + 1, dtype=float)
+        if x_boundary == -1:
+            x_boundary = (x_points - 1) / 2
+        if y_boundary == -1:
+            y_boundary = (y_points - 1) / 2
+
+        x = onp.linspace(-x_boundary, x_boundary, x_points, dtype=float)
+        y = onp.linspace(-y_boundary, y_boundary, y_points, dtype=float)
 
         grid = onp.stack(onp.meshgrid(x, y), axis=2)
         self.data_pool = grid.reshape(-1, grid.shape[-1])
