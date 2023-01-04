37e3ce4ea5ee561268270be4d7f4cbd92b0a3ff0
S.Tovey <tovey.samuel@gmail.com>
2023-01-04 09:54:53 +0100

    add integration test and resolve failing test in CI.

---
+++ CI/integration_tests/model_recording/test_model_recording_deployment.py
@@ -0,0 +1,136 @@
+"""
+ZnRND: A zincwarecode package.
+
+License
+-------
+This program and the accompanying materials are made available under the terms
+of the Eclipse Public License v2.0 which accompanies this distribution, and is
+available at https://www.eclipse.org/legal/epl-v20.html
+
+SPDX-License-Identifier: EPL-2.0
+
+Copyright Contributors to the zincwarecode Project.
+
+Contact Information
+-------------------
+email: zincwarecode@gmail.com
+github: https://github.com/zincware
+web: https://zincwarecode.com/
+
+Citation
+--------
+If you use this module please cite us with:
+
+Summary
+-------
+Test the model recording.
+"""
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+import numpy as np
+import optax
+from neural_tangents import stax
+
+import znrnd as rnd
+
+
+class TestRecorderDeployment:
+    """
+    Test suite for the model recorder.
+
+    In the setup, we download the data, prepare the recorders and train a model on
+    a small data set. The rest of the test is then about checking how the recording
+    has turned out.
+    """
+
+    @classmethod
+    def setup_class(cls):
+        """
+        Prepare the class for running.
+        """
+        # Data Generator
+        data_generator = rnd.data.MNISTGenerator(ds_size=10)
+
+        # Make a network
+        network = stax.serial(
+            stax.Flatten(), stax.Dense(128), stax.Relu(), stax.Dense(10)
+        )
+
+        # Set the class assigned recorders
+        cls.train_recorder = rnd.model_recording.JaxRecorder(
+            loss=True, accuracy=True, update_rate=1
+        )
+        cls.test_recorder = rnd.model_recording.JaxRecorder(
+            loss=True, accuracy=True, update_rate=5
+        )
+
+        # Define the model
+        production_model = rnd.models.NTModel(
+            nt_module=network,
+            optimizer=optax.adam(learning_rate=0.01),
+            loss_fn=rnd.loss_functions.CrossEntropyLoss(),
+            input_shape=(1, 28, 28, 1),
+            accuracy_fn=rnd.accuracy_functions.LabelAccuracy(),
+        )
+
+        cls.train_recorder.instantiate_recorder(
+            data_length=10, data_set=data_generator.train_ds
+        )
+        cls.test_recorder.instantiate_recorder(
+            data_length=10, data_set=data_generator.test_ds
+        )
+
+        # Train the model with the recorders
+        cls.batch_wise_metrics = production_model.train_model(
+            train_ds=data_generator.train_ds,
+            test_ds=data_generator.test_ds,
+            batch_size=5,
+            recorders=[cls.train_recorder, cls.test_recorder],
+            epochs=10,
+        )
+
+    def test_batch_loss(self):
+        """
+        Test that the batch_wise_metrics are returned correctly.
+        """
+        assert np.sum(self.batch_wise_metrics["train_accuracy"]) > 0
+        assert len(self.batch_wise_metrics["train_accuracy"]) == 10
+        assert np.sum(self.batch_wise_metrics["train_losses"]) > 0
+        assert len(self.batch_wise_metrics["train_losses"]) == 10
+
+    def test_private_arrays(self):
+        """
+        Test that the recorder internally holds the correct values.
+        """
+        assert len(self.train_recorder._loss_array) == 10
+        assert np.sum(self.train_recorder._loss_array) > 0
+        assert len(self.train_recorder._accuracy_array) == 10
+        assert np.sum(self.train_recorder._accuracy_array) > 0
+
+        assert len(self.test_recorder._loss_array) == 10
+        assert len(self.test_recorder._accuracy_array) == 10
+        assert np.sum(self.test_recorder._loss_array) > 0
+        assert np.sum(self.test_recorder._accuracy_array) > 0
+
+        # Test that the sample rate is correct
+        assert np.sum(self.test_recorder._accuracy_array[2:]) == 0.0
+
+    def test_export_function(self):
+        """
+        Test that the reports are exported correctly.
+        """
+        train_report = self.train_recorder.export_dataset()
+        test_report = self.test_recorder.export_dataset()
+
+        assert len(train_report.loss) == 10
+        assert np.sum(train_report.loss) > 0
+        assert len(train_report.accuracy) == 10
+        assert np.sum(train_report.accuracy) > 0
+
+        # Arrays should be resized now.
+        assert len(test_report.loss) == 2
+        assert np.sum(test_report.loss) > 0
+        assert len(test_report.accuracy) == 2
+        assert np.sum(test_report.accuracy) > 0
