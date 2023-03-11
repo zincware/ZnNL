d3abb79b34a7019a637e87a07b0e2e8ca1674522
knikolaou <>
2023-03-11 17:30:24 +0100

    Add a test for the parameter adaption to all unit tests of training strategies.
    Each training strategy has an update_training_kwargs method, which is called inside the training decorator before executing the training of a model.
    The adaptions of training keywords performed in this method are tested for all training strategies.

--- CI/unit_tests/training_strategies/test_simple_training.py
+++ CI/unit_tests/training_strategies/test_simple_training.py
@@ -27,17 +27,81 @@ import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
+from typing import Callable, List, Optional, Union
+
+import optax
 from jax import random
+from neural_tangents import stax
 from numpy.testing import assert_raises
 
+from znrnd.accuracy_functions import AccuracyFunction
 from znrnd.loss_functions import MeanPowerLoss
-from znrnd.training_strategies import SimpleTraining
+from znrnd.models.jax_model import JaxModel
+from znrnd.models.nt_model import NTModel
+from znrnd.training_recording import JaxRecorder
+from znrnd.training_strategies import RecursiveMode, SimpleTraining
+from znrnd.training_strategies.training_decorator import train_func
+
+
+class SimpleDecoratorTester(SimpleTraining):
+    """
+    Class to test the training decorator of the simple training.
+    """
+
+    def __init__(
+        self,
+        model: Union[JaxModel, None],
+        loss_fn: Callable,
+        accuracy_fn: AccuracyFunction = None,
+        seed: int = None,
+        recursive_mode: RecursiveMode = None,
+        disable_loading_bar: bool = False,
+        recorders: List["JaxRecorder"] = None,
+    ):
+        super().__init__(
+            model=model,
+            loss_fn=loss_fn,
+            accuracy_fn=accuracy_fn,
+            seed=seed,
+            recursive_mode=recursive_mode,
+            disable_loading_bar=disable_loading_bar,
+            recorders=recorders,
+        )
+
+    @train_func
+    def train_model(
+        self,
+        train_ds: dict,
+        test_ds: dict,
+        epochs: Optional[Union[int, List[int]]] = None,
+        batch_size: Optional[Union[int, List[int]]] = None,
+        **kwargs,
+    ):
+        """
+        Define train method to test how the decorator changes the inputs.
+
+        Returns
+        -------
+        Epochs and batch_size of the simple training strategy.
+        """
+        return epochs, batch_size
 
 
 class TestSimpleTraining:
     """
     Unit test suite for the simple training strategy.
     """
+
+    @classmethod
+    def setup_class(cls):
+        """
+        Create data for the tests.
+        """
+        key1, key2 = random.split(random.PRNGKey(1), 2)
+        x = random.normal(key1, (3, 8))
+        y = random.normal(key1, (3, 1))
+        cls.train_ds = {"inputs": x, "targets": y}
+        cls.test_ds = {"inputs": x, "targets": y}
 
     def test_model_error(self):
         """
@@ -50,17 +114,50 @@ class TestSimpleTraining:
         executed.
         """
 
-        # Create some test data
-        key1, key2 = random.split(random.PRNGKey(1), 2)
-        x = random.normal(key1, (3, 8))
-        y = random.normal(key1, (3, 1))
-        train_ds = {"inputs": x, "targets": y}
-        test_ds = train_ds
-
         trainer = SimpleTraining(
             model=None,
             loss_fn=MeanPowerLoss(order=2),
             disable_loading_bar=True,
         )
 
-        assert_raises(KeyError, trainer.train_model, train_ds, test_ds, 1)
+        assert_raises(KeyError, trainer.train_model, self.train_ds, self.test_ds, 1)
+
+    def test_update_training_kwargs(self):
+        """
+        Test the parameter adaption of the training strategy when executing the
+        training.
+
+        When calling the train_model method, parameters get adapted if necessary.
+        This methods checks if the adaption is done correctly.
+        """
+        model = NTModel(
+            nt_module=stax.serial(stax.Dense(5), stax.Relu(), stax.Dense(1)),
+            optimizer=optax.adam(learning_rate=0.001),
+            input_shape=(1, 8),
+        )
+
+        simple_tester = SimpleDecoratorTester(
+            model=model,
+            loss_fn=MeanPowerLoss(order=2),
+        )
+
+        # Test if the default batch size and epochs correct.
+        epochs, batch_size = simple_tester.train_model(
+            train_ds=self.train_ds,
+            test_ds=self.train_ds,
+        )
+        assert epochs == 50
+        assert batch_size == len(self.train_ds["inputs"])
+
+        # Test if the epochs and the batch_siza are changed correctly.
+        epochs, batch_size = simple_tester.train_model(
+            train_ds=self.train_ds, test_ds=self.train_ds, epochs=100, batch_size=1
+        )
+        assert epochs == 100
+        assert batch_size == 1
+
+        # Test if the batch size is adapted to the size of available data.
+        epochs, batch_size = simple_tester.train_model(
+            train_ds=self.train_ds, test_ds=self.train_ds, batch_size=100
+        )
+        assert batch_size == len(self.train_ds["inputs"])
