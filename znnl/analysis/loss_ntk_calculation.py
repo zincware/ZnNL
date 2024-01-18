930cac8e6aaca9f667e9a0776c193525bb61fd82
m-sauter <sauterm6800@gmail.com>
2024-01-18 18:58:46 +0100

    Renaming file

--- znnl/analysis/loss_ntk_calculation.py
+++ znnl/analysis/loss_ntk_calculation.py
@@ -26,21 +26,37 @@ Summary
 """
 
 import neural_tangents as nt
+from typing import Callable
 
 
-class ntk_calculation:
-    def function_for_loss_ntk(params)
-    def calculate_loss_ntk(
+class loss_ntk_calculation:
+    def __init__(
         self,
-        model,
-        metric_fn,
-        dataset,
+        metric_fn: Callable,
+        dataset: dict,
         ntk_batch_size: int = 10,
         store_on_device: bool = True,
     ):
+        """Constructor for the loss ntk calculation class."""
+        self.metric_fn = metric_fn
+        self.dataset = dataset
+        self.ntk_batch_size = ntk_batch_size
+        self.store_on_device = store_on_device
+
+    def _function_for_loss_ntk_helper(params, dataset, metric_fn, apply_fn):
+        return metric_fn(apply_fn(params, dataset["inputs"]), dataset["targets"])
+
+    def calculate_loss_ntk(
+        self,
+        model,
+    ):
+        _function_for_loss_ntk = lambda x, y: self._function_for_loss_ntk_helper(
+            x, y, metric_fn, model._ntk_apply_fn
+        )
+
         # Prepare NTK calculation
         empirical_ntk = nt.batch(
-            nt.empirical_ntk_fn(f=model._ntk_apply_fn, trace_axes=trace_axes),
+            nt.empirical_ntk_fn(f=_function_for_loss_ntk, trace_axes=trace_axes),
             batch_size=ntk_batch_size,
             store_on_device=store_on_device,
         )
