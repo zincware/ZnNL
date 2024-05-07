c5e1170613ec80b29d8b1b0acd2b5801520a8104
SamTov <tovey.samuel@gmail.com>
2024-05-07 11:33:36 +0200

    remove jit

--- znnl/models/jax_model.py
+++ znnl/models/jax_model.py
@@ -122,7 +122,7 @@ class JaxModel:
 
         # Prepare NTK calculation
         self.empirical_ntk = nt.batch(
-            nt.empirical_ntk_fn(f=jax.jit(self._ntk_apply_fn), trace_axes=trace_axes),
+            nt.empirical_ntk_fn(f=self._ntk_apply_fn, trace_axes=trace_axes),
             batch_size=ntk_batch_size,
             store_on_device=store_on_device,
         )
